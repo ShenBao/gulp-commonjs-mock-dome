ae1c263a53ff1ec516a1e0441299b6a26f9828bd
shenbao <shenbaoone@gmail.com>
2017-03-23 17:08:16 +0800

    mock.js

--- mock.js
+++ mock.js
@@ -5,13 +5,13 @@ exports.mock = function (req, res, next) {
     var urlObj = url.parse(req.url, true);
     switch (urlObj.pathname) {
         case '/api/list.json':
-        res.setHeader('Content-type', 'application/json');
-        fs.readFile('./mock/project/package.json', 'utf-8', function (err, data) {
-            res.end(data);
-        });
+            res.setHeader('Content-type', 'application/json');
+            fs.readFile('./mock/project/package.json', 'utf-8', function (err, data) {
+                res.end(data);
+            });
         return;
         case '/api/getuser.json':
-        // ....
+            // ....
         return;
     }
     next();
